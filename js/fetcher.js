3800bb2a777419b12db0d841bb9e2c5cf8abf6c0
DeboraRivas1805 <00059922@uca.edu.sv>
2024-03-23 13:38:43 -0600

    Solo el dashboard, please :D

--- js/fetcher.js
+++ js/fetcher.js
@@ -1,8 +1,8 @@
 fetch('./header.html')
-.then(response => response.text())
-.then(html => {
-  document.getElementById('header-container').innerHTML =html; //dentro del argumento del getElementById debe ir el id del div donde quieremos que se inserte lo que estamos fetching
-}) 
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('header-container').innerHTML =html; //dentro del argumento del getElementById debe ir el id del div donde quieremos que se inserte lo que estamos fetching
+  }) ;
 
 fetch('./footer.html')
 .then(response => response.text())
