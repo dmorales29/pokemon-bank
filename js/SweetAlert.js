4527fd58b927c5031e48f160a67b9840a2bb4824
jorgeee0-e <gonzalezmenjivar.5@gmail.com>
2024-03-23 23:51:24 -0600

    final version historico & Mis Finanzas, full responsive

--- js/SweetAlert.js
+++ js/SweetAlert.js
@@ -1,9 +1,9 @@
-document
-  .getElementById("loginId")
-  .onclick(
-    swal(
-      "Error",
-      "Has ingresado mal tu usario o contraseña, por favor intenta de nuevo.",
-      "error"
-    )
-  );
+document.getElementById('loginId').addEventListener("click",function(event){
+  event.preventDefault();
+  swal(
+    "Error",
+    "Has ingresado mal tu usario o contraseña, por favor intenta de nuevo.",
+    "error"
+  )
+})
+  
